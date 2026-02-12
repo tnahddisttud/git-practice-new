7f7de52ede00427c53049ce1d1367b3713d7fb88
tnahddisttud <siddhantp73@gmail.com>
2026-02-12 18:45:32 +0530

    change in main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def print_hi(name):
 
 if __name__ == '__main__':
     # some awesome feature for feature-1 branch
+    # some new changes also done for merge conflict
     #some new changes in main
     name = input('Enter your name: ')
     print_hi(name)
